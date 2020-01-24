6a67956cb8a2f979903a650a36ab1d3169f525f4
John Gomes <jgomes@apterainc.com>
2020-01-24 15:36:51 -0500

    Add CatalogApi with RabbitMQ and DockerCompose file

---
+++ Catalog.Api/Infrastructure/CatalogContext.cs
@@ -0,0 +1,36 @@
+﻿namespace Microsoft.eShopOnContainers.Services.Catalog.API.Infrastructure
+{
+    using Microsoft.EntityFrameworkCore;
+    using EntityConfigurations;
+    using Model;
+    using Microsoft.EntityFrameworkCore.Design;
+
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+        }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new CatalogBrandEntityTypeConfiguration());
+            builder.ApplyConfiguration(new CatalogTypeEntityTypeConfiguration());
+            builder.ApplyConfiguration(new CatalogItemEntityTypeConfiguration());
+        }     
+    }
+
+
+    public class CatalogContextDesignFactory : IDesignTimeDbContextFactory<CatalogContext>
+    {
+        public CatalogContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder =  new DbContextOptionsBuilder<CatalogContext>()
+                .UseSqlServer("server=localhost,1434;user id=sa;password=Pass@word123;database=Microsoft.eShopOnContainers.Services.CatalogDb;");
+
+            return new CatalogContext(optionsBuilder.Options);
+        }
+    }
+}
